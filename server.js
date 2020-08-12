d750a36cbfa54897a39ed32159dd9dc4b95233d4
thisaramadawalage <tmadawalage@yahoo.com>
2020-08-12 17:20:59 +0530

    Add procfile. Modify server.js to include a dyanamic port.

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ app.get("/",function (req, res) {
     res.sendFile(__dirname + "/index.html");
 })
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("Server online on port 3000");
 });
